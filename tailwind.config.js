d78755ad9996a1034a462fdc24674d0375ea8230
BossBele <adambeleko@gmail.com>
2021-02-14 19:47:46 +0300

    tailwind css config add

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: {
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    options: {
+      keyframes: true,
+      fontFace: true,
+    },
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
