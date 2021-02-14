332ae8a90373b897d8e817fe6f2e13ad367303ad
BossBele <adambeleko@gmail.com>
2021-02-14 19:48:09 +0300

    footer component

---
+++ components/Footer.js
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+
+export default function Footer({}) {
+  return (
+    <footer className="">
+      <Socials />
+    </footer>
+  );
+}
+
+function Socials({}) {
+  useEffect(() => {}, []);
+  return <></>;
+}
