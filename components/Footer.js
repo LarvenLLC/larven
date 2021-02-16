b6bf758f7b6ec1cc2bfc356daeedc44cf1e936de
BossBele <adambeleko@gmail.com>
2021-02-16 17:29:29 +0300

    added footer columns

--- components/Footer.js
+++ components/Footer.js
@@ -1,14 +1,148 @@
 import { useEffect } from "react";
 
+import moment from "moment";
+
 export default function Footer({}) {
   return (
     <footer className="">
-      <Socials />
+      <section className="grid grid-cols-1 md:grid-cols-4 gap-6 p-5">
+        <div className="">
+          <h4>Coorporate</h4>
+          <ul className="list-inside">
+            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit</li>
+          </ul>
+        </div>
+        <div className="">
+          <h4>Coorporate</h4>
+          <ul className="list-inside">
+            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit</li>
+          </ul>
+        </div>
+        <div className="">
+          <h4>Coorporate</h4>
+          <ul className="list-inside">
+            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit</li>
+          </ul>
+        </div>
+        <div className="">
+          <h4>Socials</h4>
+          <Socials />
+          <hr />
+        </div>
+      </section>
+      <section className="text-center">
+        <small>© {moment().year()} Larven. All Rights Reserved.</small>
+      </section>
     </footer>
   );
 }
 
 function Socials({}) {
   useEffect(() => {}, []);
-  return <></>;
+  return (
+    <>
+      <div className="grid grid-cols-5 gap-4 py-4">
+        {/* linkedIn */}
+        <div>
+          <a href="">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-linkedin"
+            >
+              <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
+              <rect x="2" y="9" width="4" height="12"></rect>
+              <circle cx="4" cy="4" r="2"></circle>
+            </svg>
+          </a>
+        </div>
+        {/* twitter */}
+        <div>
+          <a href="">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-twitter"
+            >
+              <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
+            </svg>
+          </a>
+        </div>
+        {/* facebook */}
+        <div>
+          <a href="">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-facebook"
+            >
+              <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
+            </svg>
+          </a>
+        </div>
+        {/* instagram */}
+        <div>
+          <a href="">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-instagram"
+            >
+              <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
+              <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
+              <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
+            </svg>
+          </a>
+        </div>
+        {/* youtube */}
+        <div>
+          <a href="">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-youtube"
+            >
+              <path d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z"></path>
+              <polygon points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02"></polygon>
+            </svg>
+          </a>
+        </div>
+      </div>
+    </>
+  );
 }
