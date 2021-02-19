22455ade039b13e9db341e0d008fd0e50b9bdde5
BossBele <adambeleko@gmail.com>
2021-02-19 22:16:22 +0300

    styling

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
         {/* carousel section */}
         <Carousel />
         {/* testimonials section */}
-        <section className="px-4 sm:px-6 lg:py-16 lg:px-8">
+        <section className="">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-16">
             <div className="relative overflow-hidden h-96 w-100 rounded-lg">
               <Image
@@ -88,7 +88,7 @@ export default function Home() {
         {/* features section */}
         <section>
           <div className="py-12 bg-white">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="mx-auto">
               <div className="lg:text-center">
                 <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
                   Transactions
@@ -239,7 +239,7 @@ export default function Home() {
         {/* cta section */}
         <section>
           <div className="bg-gray-50">
-            <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+            <div className="mx-auto py-12 lg:flex lg:items-center lg:justify-between">
               <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
                 <span className="block">Ready to dive in?</span>
                 <span className="block text-indigo-600">
