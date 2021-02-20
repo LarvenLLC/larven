385e9f19ecc7305c46bf2c5da7af3766251d65bc
BossBele <adambeleko@gmail.com>
2021-02-20 20:17:30 +0300

    about copy

---
+++ pages/about.js
@@ -0,0 +1,51 @@
+import Layout from "../components/Layout";
+
+export default function About({}) {
+  return (
+    <Layout>
+      {/* purpose section */}
+      <section className="text-center">
+        <h2>Our Purpose</h2>
+        <p>To uplift mankind with technology they can own</p>
+        <p>
+          We embrace the power of technology to bring happiness to our clients,
+          people and communities.
+        </p>
+      </section>
+      {/* numbers section */}
+      {/* values section */}
+      <section className="flex flex-wrap justify-center gap-10">
+        <div className="">
+          <h2>Our Values</h2>
+          <ul>
+            <li>Human Centered Design</li>
+            <li>Humane Development</li>
+            <li>Specialization &amp; Division of Labour</li>
+          </ul>
+        </div>
+        {/* pillars section */}
+        <div>
+          <h2>Our Pillars</h2>
+          <ul>
+            <li>Human Centered Design</li>
+            <li>Humane Development</li>
+            <li>Specialization &amp; Division of Labour</li>
+          </ul>
+        </div>
+      </section>
+      {/* accomplishments section */}
+      {/* how we work section */}
+      <section>
+        <div className="flex flex-wrap items-center justify-center gap-10">
+          <h1>How we work</h1>
+          <ul>
+            <li>Total inclusiveness</li>
+            <li>Humane Development</li>
+            <li>Specialization &amp; Division of Labour</li>
+          </ul>
+        </div>
+      </section>
+      {/* awards & recognition section */}
+    </Layout>
+  );
+}
