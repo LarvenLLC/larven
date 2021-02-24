3129252f4d4dd671fbd3a0b50caf71f6ee51c3fe
BossBele <adambeleko@gmail.com>
2021-02-24 16:44:01 +0300

    added events

---
+++ pages/events.js
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+
+export default function Events({}) {
+  return (
+    <Layout>
+      <section>
+        <h3>Events</h3>
+        <div>Under Construction</div>
+      </section>
+    </Layout>
+  );
+}
