623ba7860cafc74f5eebb3e31219a8859667c0a9
BossBele <adambeleko@gmail.com>
2021-02-24 16:43:49 +0300

    added careers

---
+++ pages/careers.js
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+
+export default function Careers({}) {
+  return (
+    <Layout>
+      <section>
+        <h3>Careers</h3>
+        <div>Under Construction</div>
+      </section>
+    </Layout>
+  );
+}
