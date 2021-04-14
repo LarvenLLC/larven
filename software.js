972e9b368886f05f1b7d482eac03efb0ec3967f7
BossBele <adambeleko@gmail.com>
2021-04-14 22:20:17 +0300

    software

--- software.js
+++ software.js
@@ -24,36 +24,160 @@ const software = [
           "Asset management refers to a systematic approach to the governance and realization of value from the things that a group or entity is responsible for, over their whole life cycles. It may apply both to tangible assets and to intangible assets.",
       },
     ],
-    keywords: ["e-commerce", "utility", "retail", "wholesale", "consumer"],
+    keywords: ["retail", "wholesale", "pharmacy", "medicine"],
   },
   {
     deployed: false,
-    name: "Inventory Management System",
-    benefits: [{ benefit: "", description: "" }],
-    keywords: ["e-commerce", "utility", "retail", "wholesale", "consumer"],
+    name: "Invoicing System",
+    benefits: [
+      {
+        benefit: "Records",
+        description:
+          "Not having up-to-date and accurate archives of invoices issued and received can risk loss of funds and hefty fines from authorities.",
+      },
+      {
+        benefit: "Payment Prompt",
+        description:
+          "Invoices remind clients of the work completed or goods provided.",
+      },
+      {
+        benefit: "Customer Relations",
+        description:
+          "Efficient invoice generation and payment collection can improve customer satisfaction. Equally, poorly managed systems can easily damage a company’s reputation.",
+      },
+      {
+        benefit: "Brand Identity",
+        description:
+          "You can add your logo, color scheme, and fonts that matches your brand’s voice. When you strengthen your identity you increase the chances of getting paid on-time. It’s been found that including your logo increases payments by 3x. You can also create customized messages, such as a “Thank You!” You will be able to upsell new products or services.",
+      },
+    ],
+    keywords: ["utility"],
   },
   {
     deployed: false,
-    name: "Inventory Management System",
-    benefits: [{ benefit: "", description: "" }],
-    keywords: ["e-commerce", "utility", "retail", "wholesale", "consumer"],
+    name: "Courier/Parcel Management System",
+    benefits: [
+      {
+        benefit: "Records",
+        description:
+          "Records sales, items, locations and benefit from this data to enhance your business.",
+      },
+      {
+        benefit: "Automation",
+        description:
+          "Automated processes including customer notifications, check-in and check-out. Let the software manage delivery from start to end.",
+      },
+      {
+        benefit: "Customer Relations",
+        description:
+          "Keep customer satisfied with extensive feedback about their parcels. Let them be part of the journey.",
+      },
+      {
+        benefit: "Dispute Handling",
+        description:
+          "Have the best information to intelligently handle disputes. Know who to hold accountable and make the best decisions that would not harm your business reputation.",
+      },
+    ],
+    keywords: [
+      "couriers",
+      "buses",
+      "transport",
+      "automobile",
+      "airlines",
+      "delivery",
+    ],
   },
   {
     deployed: false,
-    name: "Inventory Management System",
-    benefits: [{ benefit: "", description: "" }],
-    keywords: ["e-commerce", "utility", "retail", "wholesale", "consumer"],
+    name: "Order Management System",
+    benefits: [
+      {
+        benefit: "Highly Personalised Experience",
+        description:
+          "Use data to learn more about your guests allowing you to deliver seamless customer experiences.",
+      },
+      {
+        benefit: "Efficient Workflows",
+        description:
+          "Help your team members do their jobs better. An intelligent system means fewer taps to close out a table, resulting in faster and efficient customer service.",
+      },
+      {
+        benefit: "Recording & Reports",
+        description:
+          "Easily and effectively store order records in a secure way. Generate instant reports from these data to gain insights on your business.",
+      },
+    ],
+    keywords: ["restaurant", "food distribution", "food", "food hospitality"],
   },
   {
     deployed: false,
-    name: "Inventory Management System",
-    benefits: [{ benefit: "", description: "" }],
-    keywords: ["e-commerce", "utility", "retail", "wholesale", "consumer"],
+    name: "E-Commerce Web Application",
+    benefits: [
+      {
+        benefit: "Payment Integration",
+        description:
+          "Allow customers to make pay on the web and easily receive order payments for further processing.",
+      },
+      {
+        benefit: "Blogging",
+        description:
+          "Grow your online business with actionable marketing and selling advice, tips, and resources.",
+      },
+      {
+        benefit: "Items Management",
+        description:
+          "Add, update and delete store items through an administration platform. Moreover, categorize items to your customers' benefit.",
+      },
+      {
+        benefit: "Order Management",
+        description:
+          "Get notified of incoming orders and see them through an administration platform. Easily take on orders from there.",
+      },
+    ],
+    keywords: ["e-commerce", "retail", "wholesale"],
   },
   {
     deployed: false,
-    name: "Inventory Management System",
-    benefits: [{ benefit: "", description: "" }],
-    keywords: ["e-commerce", "utility", "retail", "wholesale", "consumer"],
+    name: "SIM Banking Application",
+    benefits: [
+      {
+        benefit: "Secure & Free",
+        description:
+          "The app is made to be secure by encrypting key variables and hiding sensitive files. You can also add your security rules in the code. Easily integrate your database.",
+      },
+      {
+        benefit: "Plug-in Ready",
+        description:
+          "Get an app optimized for your in-house system requirements. Easily integrate your database.",
+      },
+      {
+        benefit: "Quality Assurance",
+        description:
+          "Each part of the program is tested. App is made to follow the best Development-Operations practices.",
+      },
+    ],
+    keywords: ["banking", "finance"],
+  },
+  {
+    deployed: false,
+    name: "Loan Management System",
+    benefits: [
+      {
+        benefit: "Secure & Free",
+        description:
+          "The app is made to be secure by encrypting key variables and hiding sensitive files. You can also add your security rules in the code. Easily integrate your database.",
+      },
+      {
+        benefit: "Plug-in Ready",
+        description:
+          "Get an app optimized for your in-house system requirements. Easily integrate your database.",
+      },
+      {
+        benefit: "Quality Assurance",
+        description:
+          "Each part of the program is tested. App is made to follow the best Development-Operations practices.",
+      },
+    ],
+    keywords: ["banking", "finance", "microfinance"],
   },
 ];
