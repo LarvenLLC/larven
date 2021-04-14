55ab7713a45c54f38deb5f9ce2b19f09ff6b6608
BossBele <adambeleko@gmail.com>
2021-04-14 20:37:19 +0300

    IMS

--- software.js
+++ software.js
@@ -8,6 +8,21 @@ const software = [
         description:
           "Supply chain management is the management of the flow of goods and services and includes all processes that transform raw materials into final products.",
       },
+      {
+        benefit: "warehouse management system",
+        description:
+          "Warehouse management system is a software application designed to support and optimize warehouse functionality and distribution center management.",
+      },
+      {
+        benefit: "Forecasting",
+        description:
+          "Forecasting is the process of making predictions based on past and present data and most commonly by analysis of trends. A commonplace example might be estimation of some variable of interest at some specified future date.",
+      },
+      {
+        benefit: "Asset management",
+        description:
+          "Asset management refers to a systematic approach to the governance and realization of value from the things that a group or entity is responsible for, over their whole life cycles. It may apply both to tangible assets and to intangible assets.",
+      },
     ],
     keywords: ["e-commerce", "utility", "retail", "wholesale", "consumer"],
   },
