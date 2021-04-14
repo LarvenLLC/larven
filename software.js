75cf6e7761b1e678945b11745efa0f42f894f20e
BossBele <adambeleko@gmail.com>
2021-04-14 20:17:26 +0300

    software data

---
+++ software.js
@@ -0,0 +1,38 @@
+const software = [
+  {
+    deployed: false,
+    name: "Inventory Management System",
+    benefits: [{ benefit: "", description: "" }],
+    keywords: ["e-commerce", "utility", "retail", "wholesale", "consumer"],
+  },
+  {
+    deployed: false,
+    name: "Inventory Management System",
+    benefits: [{ benefit: "", description: "" }],
+    keywords: ["e-commerce", "utility", "retail", "wholesale", "consumer"],
+  },
+  {
+    deployed: false,
+    name: "Inventory Management System",
+    benefits: [{ benefit: "", description: "" }],
+    keywords: ["e-commerce", "utility", "retail", "wholesale", "consumer"],
+  },
+  {
+    deployed: false,
+    name: "Inventory Management System",
+    benefits: [{ benefit: "", description: "" }],
+    keywords: ["e-commerce", "utility", "retail", "wholesale", "consumer"],
+  },
+  {
+    deployed: false,
+    name: "Inventory Management System",
+    benefits: [{ benefit: "", description: "" }],
+    keywords: ["e-commerce", "utility", "retail", "wholesale", "consumer"],
+  },
+  {
+    deployed: false,
+    name: "Inventory Management System",
+    benefits: [{ benefit: "", description: "" }],
+    keywords: ["e-commerce", "utility", "retail", "wholesale", "consumer"],
+  },
+];
