6808097bb7781b38b3e49d56b54bc796360bcb18
BossBele <adambeleko@gmail.com>
2021-04-18 17:09:48 +0300

    meta data

--- components/Title.js
+++ components/Title.js
@@ -19,6 +19,33 @@ export default function Title({ title = "" }) {
   title;
   return (
     <Head>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/public/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/public/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/public/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/public/site.webmanifest" />
+      <link
+        rel="mask-icon"
+        href="/public/safari-pinned-tab.svg"
+        color="#00ab44"
+      />
+      <link rel="shortcut icon" href="/public/favicon.ico" />
+      <meta name="msapplication-TileColor" content="#00ab44" />
+      <meta name="msapplication-config" content="/public/browserconfig.xml" />
+      <meta name="theme-color" content="#ffffff"></meta>
       <title>
         {`${!pathname ? "Larven | " : ""}
         ${lingo(title).title()}
