7c470d05714cd54219515cb4f756f4e068a50405
BossBele <adambeleko@gmail.com>
2021-04-23 23:57:13 +0300

    refactoring

--- pages/industry/index.js
+++ pages/industry/index.js
@@ -1,16 +1,25 @@
 import Layout from "../../components/Layout";
 import software from "../../software";
 
+const keywords = Array.isArray(software)
+  ? software?.map((el) => el?.keywords)?.flat()
+  : [];
+
 export default function Industries({}) {
-  const industries = [...new Set(software?.map((el) => el?.keywords))];
+  const industries = [...new Set(keywords)];
 
   return (
     <>
       <Layout title="">
-        <section>
-          {industries?.map((industry) => (
-            <div>{industry?.keywords ?? ""}</div>
-          ))}
+        <section className="flex items-center flex-wrap">
+          {industries?.map((industry, i) => (
+            <div
+              key={i}
+              className="px-5 py-3 border border-light text-secondary font-medium rounded-md"
+            >
+              {industry ?? ""}
+            </div>
+          )) ?? ""}
         </section>
       </Layout>
     </>
