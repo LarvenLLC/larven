e0bc1ec1a31871419d3d760a55f417e0f0189a70
BossBele <adambeleko@gmail.com>
2021-04-23 23:02:51 +0300

    styling

--- pages/index.js
+++ pages/index.js
@@ -96,7 +96,7 @@ export default function Home() {
         {/* <section className="pt-0 bg-white">
           <div className="mx-auto">
             <div className="lg:text-center">
-              <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
+              <h2 className="text-base text-primary font-semibold tracking-wide uppercase">
                 Transactions
               </h2>
               <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
@@ -246,19 +246,19 @@ export default function Home() {
           <div className="mx-auto lg:flex lg:items-center lg:justify-between">
             <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
               <span className="block">Ready to transform your processes?</span>
-              <span className="block text-indigo-600">Join us now.</span>
+              <span className="block text-primary">Join us now.</span>
             </h2>
             <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
               <div className="inline-flex rounded-md shadow">
                 <Link href="/contacts">
-                  <a className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">
+                  <a className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-primary hover:bg-indigo-700">
                     Contact Us
                   </a>
                 </Link>
               </div>
               <div className="ml-3 inline-flex rounded-md shadow">
                 <Link href="/about">
-                  <a className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50">
+                  <a className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-primary bg-white hover:bg-indigo-50">
                     Learn more
                   </a>
                 </Link>
