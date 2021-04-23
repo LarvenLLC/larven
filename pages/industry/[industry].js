301d4134b4acf194e543ae8fd193277d1de28e3d
BossBele <adambeleko@gmail.com>
2021-04-23 23:08:08 +0300

    react component

---
+++ pages/industry/[industry].js
@@ -0,0 +1,11 @@
+import Layout from "../../components/Layout";
+
+export default function Industry({}) {
+  return (
+    <>
+      <Layout title="">
+        <div />
+      </Layout>
+    </>
+  );
+}
