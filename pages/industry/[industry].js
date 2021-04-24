a70a21f06ec31ac2ffa78b57f72c997848249014
BossBele <adambeleko@gmail.com>
2021-04-24 16:52:33 +0300

    router

--- pages/industry/[industry].js
+++ pages/industry/[industry].js
@@ -8,15 +8,16 @@ import software from "../../software";
 
 export default function Industry({}) {
   const router = useRouter();
+  const { industry = "" } = router.query;
 
-  const keywords = Array.isArray(software)
-    ? software?.map((el) => el?.keywords)?.flat()
+  const systems = Array.isArray(software)
+    ? software?.filter((el) => el?.keywords?.includes(industry))
     : [];
 
   return (
     <>
       <Layout title="">
-        <section></section>
+        <section>{systems}</section>
       </Layout>
     </>
   );
