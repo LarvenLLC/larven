7d586f57d83d899d3b5875c55d0f1cb6300f5398
BossBele <adambeleko@gmail.com>
2021-04-24 16:46:45 +0300

    modules

--- pages/industry/[industry].js
+++ pages/industry/[industry].js
@@ -1,6 +1,18 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+import lingo from "lingojs";
+
 import Layout from "../../components/Layout";
+import software from "../../software";
 
 export default function Industry({}) {
+  const router = useRouter();
+
+  const keywords = Array.isArray(software)
+    ? software?.map((el) => el?.keywords)?.flat()
+    : [];
+
   return (
     <>
       <Layout title="">
