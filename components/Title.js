007b26efea7bf8a79efa088d62545aee34ff5d15
BossBele <adambeleko@gmail.com>
2021-04-24 18:50:13 +0300

    r

--- components/Title.js
+++ components/Title.js
@@ -22,29 +22,25 @@ export default function Title({ title = "" }) {
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/public/apple-touch-icon.png"
+        href="/apple-touch-icon.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/public/favicon-32x32.png"
+        href="/favicon-32x32.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/public/favicon-16x16.png"
+        href="/favicon-16x16.png"
       />
-      <link rel="manifest" href="/public/site.webmanifest" />
-      <link
-        rel="mask-icon"
-        href="/public/safari-pinned-tab.svg"
-        color="#00ab44"
-      />
-      <link rel="shortcut icon" href="/public/favicon.ico" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#00ab44" />
+      <link rel="shortcut icon" href="/favicon.ico" />
       <meta name="msapplication-TileColor" content="#00ab44" />
-      <meta name="msapplication-config" content="/public/browserconfig.xml" />
+      <meta name="msapplication-config" content="/browserconfig.xml" />
       <meta name="theme-color" content="#ffffff"></meta>
       <title>
         {`${!pathname ? "Larven | " : ""}
