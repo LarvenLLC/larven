d458ca0e33f6f907026567855771375f8b518927
BossBele <adambeleko@gmail.com>
2021-04-24 16:57:38 +0300

    jsx

--- pages/industry/[industry].js
+++ pages/industry/[industry].js
@@ -21,9 +21,11 @@ export default function Industry({}) {
           {systems?.map((system, i) => (
             <div key={i}>
               <h4>{system?.name ?? "System Name"}</h4>
-              <a className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base no-underline font-medium rounded-md text-primary bg-green-50 hover:bg-primary hover:text-white">
-                {lingo(system ?? "").title()}
-              </a>
+              <div className="flex items-center flex-wrap gap-5">
+                <a className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base no-underline font-medium rounded-md text-primary bg-green-50 hover:bg-primary hover:text-white">
+                  {lingo(system ?? "").title()}
+                </a>
+              </div>
             </div>
           )) ?? ""}
         </section>
