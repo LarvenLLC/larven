d37f574def887bf8790bc728c179dac0c5b9df3c
BossBele <adambeleko@gmail.com>
2021-04-24 16:54:46 +0300

    jsx

--- pages/industry/[industry].js
+++ pages/industry/[industry].js
@@ -17,7 +17,15 @@ export default function Industry({}) {
   return (
     <>
       <Layout title="">
-        <section>{systems}</section>
+        <section>
+          {systems?.map((system, i) => (
+            <div key={i}>
+              <a className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base no-underline font-medium rounded-md text-primary bg-green-50 hover:bg-primary hover:text-white">
+                {lingo(system ?? "").title()}
+              </a>
+            </div>
+          )) ?? ""}
+        </section>
       </Layout>
     </>
   );
