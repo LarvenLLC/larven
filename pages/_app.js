50bf4c54a2b2e384e7114776bdcb76bac10bf209
BossBele <adambeleko@gmail.com>
2021-11-12 15:19:05 +0300

    add axios

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import "../styles/globals.css";
 
+import axios from "axios";
+
+const Axios = axios.create({
+  baseURL: '/api/',
+  timeout: 3000,
+});
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
