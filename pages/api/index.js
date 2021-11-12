4ec79735db9669c7e703bb0ce3b5c2d46ae6f8ab
BossBele <adambeleko@gmail.com>
2021-11-12 21:35:00 +0300

    enhancement

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import axios from "axios";
+import axios from 'axios';
 
 const Axios = axios.create({
   baseURL: '/api/',
