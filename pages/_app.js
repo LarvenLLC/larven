c3dab7892813130f44f70ff79cd63abc3aa89e73
BossBele <adambeleko@gmail.com>
2021-11-12 17:29:14 +0300

    rm axios

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,5 @@
 import "../styles/globals.css";
 
-import axios from "axios";
-
-const Axios = axios.create({
-  baseURL: '/api/',
-  timeout: 3000,
-});
-
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
