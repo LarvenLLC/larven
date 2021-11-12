692ae83425a2ed172bf0289f6ff2c3572b6fea16
BossBele <adambeleko@gmail.com>
2021-11-12 21:34:38 +0300

    enhancement

--- pages/api/mailchimp/members.js
+++ pages/api/mailchimp/members.js
@@ -3,38 +3,38 @@ import mailchimp from '.';
 export default async function handler(req, res) {
   const {
     method,
-    body: data
+    body: data,
   } = req;
 
-  const { email } = data;
+  const {email} = data;
 
   let response = {};
   switch (method) {
-  case 'POST': {
+    case 'POST': {
     // add subscriber to list
-    response = await mailchimp.lists.addListMember(process.env.MAILCHIMP_AUDIENCE_ID, {
-      email_address: email,
-      status: "subscribed",
-      tags: ["audit"]
-    });
-    break;
-  }
-  case 'GET':
+      response = await mailchimp.lists.addListMember(process.env.MAILCHIMP_AUDIENCE_ID, {
+        email_address: email,
+        status: 'subscribed',
+        tags: ['audit'],
+      });
+      break;
+    }
+    case 'GET':
     // get subscriber
-    break;
-  case 'PUT':
+      break;
+    case 'PUT':
     // update subscriber
-    break;
-  case 'DELETE':
+      break;
+    case 'DELETE':
     // remove subscriber from list
-    response = await mailchimp.lists.deleteListMember(
-      process.env.MAILCHIMP_AUDIENCE_ID,
-      email
-    );
-    break;
-  default:
-    res.setHeader('Allow', ['DELETE', 'GET', 'POST', 'PUT']);
-    res.status(405).end(`Method ${method} Not Allowed`);
+      response = await mailchimp.lists.deleteListMember(
+          process.env.MAILCHIMP_AUDIENCE_ID,
+          email,
+      );
+      break;
+    default:
+      res.setHeader('Allow', ['DELETE', 'GET', 'POST', 'PUT']);
+      res.status(405).end(`Method ${method} Not Allowed`);
   }
 
   return res.status(200).json(response);
