f1bc0378f65f667bae491e79f6bb27085bc30984
BossBele <adambeleko@gmail.com>
2022-01-04 17:25:44 +0300

    enhancement

--- components/Announcement.js
+++ components/Announcement.js
@@ -1,8 +1,18 @@
-import { useState } from "react";
+import {useState} from 'react';
+import Link from 'next/link';
 
-export default function Announcement({}) {
+/**
+ * @param {Object} props
+ * @return {JSX}
+ */
+export default function Announcement() {
   const [announcementHidden, setAnnouncementHidden] = useState(false);
 
+  /**
+   * Function to hide announcement
+   * @param {Event} event
+   * @return {void}
+   */
   function hideAnnouncement() {
     setAnnouncementHidden(true);
   }
@@ -12,12 +22,12 @@ export default function Announcement({}) {
   }
 
   return (
-    <section>
-      <div className="bg-indigo-600">
-        <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
+    <section className='p-0'>
+      <div className="bg-gray-500">
+        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div className="flex items-center justify-between flex-wrap">
             <div className="w-0 flex-1 flex items-center">
-              <span className="flex p-2 rounded-lg bg-indigo-800">
+              <span className="flex p-2 rounded-lg bg-primary">
                 <svg
                   className="h-6 w-6 text-white"
                   xmlns="http://www.w3.org/2000/svg"
@@ -35,23 +45,25 @@ export default function Announcement({}) {
                 </svg>
               </span>
               <p className="ml-3 font-medium text-white truncate">
-                <span className="md:hidden">We announced a new product!</span>
+                <span className="md:hidden">Larven Hackathons event this January!</span>
                 <span className="hidden md:inline">
-                  Big news! We're excited to announce a brand new product.
+                  Big news! We&apos;re excited to conduct our first ever Larven Hackathons event.
                 </span>
               </p>
             </div>
             <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
-              <a
-                href="#"
-                className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50"
-              >
+              <Link href='' passHref>
+                <button
+                  className="button-primary-light"
+                >
                 Learn more
-              </a>
+                </button>
+              </Link>
             </div>
             <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
               <button
-                className="-mr-1 flex p-2 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2"
+                className="-mr-1 flex p-2 rounded-md hover:bg-primary hover:bg-opacity-60
+                focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2"
                 onClick={hideAnnouncement}
               >
                 <span className="sr-only">Dismiss</span>
