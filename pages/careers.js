b0be5650cabae60b3a45f9601469965e40071cb1
BossBele <adambeleko@gmail.com>
2022-01-06 17:49:19 +0300

    ui optimization

--- pages/careers.js
+++ pages/careers.js
@@ -1,11 +1,28 @@
-import Layout from "../components/Layout";
+import Layout from '../components/Layout';
+import Email from '../components/Email';
 
-export default function Careers({}) {
+/**
+ * The careers page
+ * @param {object} props
+ * @return {JSX}
+ */
+export default function Careers() {
   return (
     <Layout title="Careers">
       <section>
-        <h3>Careers</h3>
-        <div>Under Construction</div>
+        <h1>Careers</h1>
+        <div className='w-full'>
+          <div className='sm:px-10 sm:border-l-[10px] lg:border-l-[14px] border-primary pb-1'>
+            <h3>Welcome to Larven!</h3>
+            <p>
+              Thank you for your interest in employment opportunities at Larven.
+              We are always looking for great talent; if you are interested in joining our team,
+              please send your CV and cover letter to <span />
+              <Email />.
+              We’ll get back to you.
+            </p>
+          </div>
+        </div>
       </section>
     </Layout>
   );
