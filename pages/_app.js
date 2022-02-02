51b7a4036855f1250982852062312ded1f9cec8f
BossBele <adambeleko@gmail.com>
2022-02-02 17:29:37 +0300

    add anonymous login

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,8 @@ import '../styles/globals.css';
 import ToastProvider from '../hooks/useToasts';
 import GoogleAnalytics from '../components/GoogleAnalytics';
 import {initializeApp} from 'firebase/app';
-import {useEffect} from 'react';
+import {getAuth, onAuthStateChanged, signInAnonymously} from 'firebase/auth';
+import {useEffect, useState} from 'react';
 
 /**
  * App component
@@ -11,13 +12,46 @@ import {useEffect} from 'react';
  * @return {function}
  */
 export default function MyApp({Component, pageProps}) {
+  const [app, setApp] = useState(null);
   useEffect(()=>{
     const firebaseConfig = JSON.parse(
         process?.env?.NEXT_PUBLIC_FIREBASE_CONFIG || '{}',
     );
     // Initialize Firebase
-    initializeApp(firebaseConfig);
+    const {name} = initializeApp(firebaseConfig);
+    setApp(name);
   }, []);
+
+  useEffect(()=>{
+    if (app) {
+      const auth = getAuth();
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          // User is signed in, see docs for a list of available properties
+          // https://firebase.google.com/docs/reference/js/firebase.User
+          // const uid = user.uid;
+          // ...
+        } else {
+          // User is signed out
+          // ...
+        }
+      });
+    }
+  }, []);
+
+  useEffect(()=>{
+    if (app) {
+      const auth = getAuth();
+      signInAnonymously(auth)
+          .then(() => {
+          // Signed in..
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+    }
+  }, [app]);
+
   return (
     <ToastProvider>
       <Component {...pageProps} />
