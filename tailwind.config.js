fca8ca0e421e4c96de870b47377e8f8aea57e785
Adam Beleko <adambeleko@gmail.com>
2023-10-13 00:58:58 +0300

    ui optimizations - tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,29 @@
+const {fontFamily} = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  mode: 'jit',
+  purge: {
+    content: [
+      './pages/**/*.{js,ts,jsx,tsx,mdx}',
+      './components/**/*.{js,ts,jsx,tsx,mdx}',
+      './app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    options: {
+      keyframes: true,
+      fontFace: true,
+    },
+  },
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ['var(--font-outfit)', ...fontFamily.sans],
+    },
     extend: {
-      colors:{
+      colors: {
+        primary: '#00AB44',
+        secondary: '#353744',
+        light: '#666666',
         'green-haze': {
           '50': '#eefff3',
           '100': '#d7ffe6',
@@ -20,14 +36,37 @@ module.exports = {
           '800': '#0a7134',
           '900': '#0a5d2d',
           '950': '#003417',
+        },
       },
-      
-
-      }
+      transitionProperty: {
+        'height': 'height',
+        'width': 'width',
+        'left': 'left',
+        'spacing': 'margin, padding',
+      },
+      keyframes: {
+        slidein: {
+          '0%': {transform: 'translateX(0px)'},
+          '100%': {transform: 'translateX(80%)'},
+        },
+      },
+      animation: {
+        slidein: 'slidein 4s ease-in-out 300ms forwards',
+      },
     },
   },
-  plugins: [
-    require('tailwind-bootstrap-grid')(),
-  ],
-}
-
+  variants: {
+    extend: {},
+  },
+  important: true,
+  plugins: [require('@tailwindcss/typography'), require('tailwind-bootstrap-grid')({
+    gridGutters: {
+      0: 0,
+      1: '0.5rem',
+      2: '1rem',
+      3: '1.5rem',
+      4: '2rem',
+      5: '3rem',
+    },
+  })],
+};
