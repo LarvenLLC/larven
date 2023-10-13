a050703b5e2aa1e3eb8d8bf47143ff3cca6b232d
Adam Beleko <adambeleko@gmail.com>
2023-10-13 15:52:58 +0300

    ui optimizations - app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import Link from "next/link";
 import Image from "next/image";
 import NavBar from "@/components/navbar";
+import Footer from "@/components/footer";
 
 const servicesData = [
   {
@@ -144,7 +144,7 @@ const Homepage = () => {
           </div>
         </div>
         <svg width="1187" height="800" viewBox="0 0 1187 800" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute -z-10 bottom-0 right-0">
-          <path d="M0 0H1187V550C1187 688.071 1075.07 800 937 800H0V0Z" fill="#00AB44" fill-opacity="0.05"/>
+          <path d="M0 0H1187V550C1187 688.071 1075.07 800 937 800H0V0Z" fill="#00AB44" fillOpacity="0.05"/>
         </svg>
       </section>
 
@@ -285,10 +285,10 @@ const Homepage = () => {
                 Hire Your Technician
               </button>
             </div>
-            <div className="col row row-cols-1 md:row-cols-2 g-5 mt-0">
+            <div className="col row row-cols-1 md:row-cols-2 md:gx-5 gy-5 mt-0">
               <div className="mt-0">
                 <div className="h-72 relative">
-                  <p className="absolute top-2 -left-28 px-5 py-2 text-sm font-bold bg-white shadow-md rounded-l-xl rounded-tr-xl text-green-haze-600">
+                  <p className="absolute top-2 -left-6 md:-left-28 px-5 py-2 text-sm font-bold bg-white shadow-md rounded-l-xl rounded-tr-xl text-green-haze-600">
                     Early Stage Startup
                   </p>
                   <Image
@@ -346,7 +346,7 @@ const Homepage = () => {
           </div>
         </div>
         <svg width="1186" height="800" viewBox="0 0 1186 800" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute -z-10 top-0 left-0">
-          <path d="M-1 0H1186V800H249C110.929 800 -1 688.071 -1 550V0Z" fill="#00AB44" fill-opacity="0.05"/>
+          <path d="M-1 0H1186V800H249C110.929 800 -1 688.071 -1 550V0Z" fill="#00AB44" fillOpacity="0.05"/>
         </svg>
       </section>
 
@@ -499,13 +499,13 @@ const Homepage = () => {
         </div>
       </section>
 
-      <section className="my-20">
+      <section className="container">
         <h4 className="text-center font-bold text-green-500">STORIES</h4>
         <h1 className="text-center font-bold text-black text-3xl mb-10">
           Our Most Popular Articles
         </h1>
 
-        <div className="grid md:grid-cols-2 gap-10 mx-20">
+        <div className="grid md:grid-cols-2 gap-10">
           <div className="space-y-10 shadow-lg rounded-lg">
             <Image
               src="/images/Frame 18.png"
@@ -521,8 +521,8 @@ const Homepage = () => {
               </h3>
               <p className="px-3">
                 Lorem ipsum dolor sit amet consectetur. Elementum nec dictumst
-                habitasse bibendum sit ut varius .Lorem ipsum dolor sit amet
-                consectetur. Elementum .
+                habitasse bibendum sit ut varius. Lorem ipsum dolor sit amet
+                consectetur. Elementum.
               </p>
               <div className="flex w-fit ml-auto items-center space-x-1 ">
                 <Image
@@ -550,10 +550,10 @@ const Homepage = () => {
               />
               <div className="space-y-3">
                 <h3 className="font-bold">
-                  As a startup should i use fire store for my app
+                  As a startup should i use firestore for my app
                 </h3>
                 <p>
-                  As a startup should i use fire store for my appLorem ipsum
+                  As a startup should i use firestore for my appLorem ipsum
                   dolor sit amet
                 </p>
                 <p className="text-green-haze-500 text-end">Gabriel Michael</p>
@@ -605,14 +605,10 @@ const Homepage = () => {
         </div>
       </section>
 
-      <section className="flex justify-center my-3">
-        <div className="relative justify-center py-16 grid md:grid-cols-2 items-center w-full bg-black text-white px-10 overflow-hidden">
-          <div className="z-1 absolute h-20 left-0 bottom-32 -rotate-45 rounded-r-full -translate-x-[100px] w-[300px] bg-green-haze-500/50" />
-          <div className="z-1 absolute h-20 left-0 bottom-0 -rotate-45 rounded-r-full -translate-x-[100px] w-[300px] bg-green-haze-500/50" />
-          <div className="z-1 absolute h-20 -rotate-45 rounded-l-full right-0 top-10 translate-x-[100px] w-[300px] bg-neutral-900" />
-          <div className="z-1 absolute h-20 -rotate-45 rounded-l-full right-0 top-30 translate-x-[100px] w-[300px] bg-neutral-900" />
-          <div className="mx-auto space-y-4 z-10">
-            <p className="text-xs">REACH OUT TO US</p>
+      <section className="container-fluid relative overflow-hidden bg-black z-0">
+        <div className="container justify-center grid md:grid-cols-2 items-center text-white">
+          <div className="mx-auto space-y-4">
+            <h4 className="text-xs">REACH OUT TO US</h4>
             <h3 className="font-bold text-2xl md:text-3xl">
               Want to Hire Great Technicians <br /> today!
             </h3>
@@ -621,7 +617,7 @@ const Homepage = () => {
               morbi pellentesque tincidunt nulla
             </p>
           </div>
-          <div className="mx-auto z-10 w-full mt-10 md:mt-0">
+          <div className="mx-auto w-full mt-10 md:mt-0">
             <form className="items-center gap-x-4 grid md:grid-cols-2 space-y-3 md:space-y-0">
               <input
                 type="email"
@@ -630,111 +626,24 @@ const Homepage = () => {
               />
               <button
                 type="submit"
-                className="px-5 py-3 text-xs text-white rounded-r-full rounded-bl-full bg-green-haze-500 w-fit ml-auto md:ml-0"
+                className="px-5 py-3 text-xs text-white rounded-r-full rounded-bl-full bg-green-haze-500 font-bold ml-auto md:ml-0"
               >
                 Get on the list
               </button>
             </form>
           </div>
         </div>
+        <svg width="165" height="313" viewBox="0 0 165 313" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute left-0 bottom-0 -z-10">
+          <path d="M129.026 35.3553C109.5 15.8291 77.8413 15.8291 58.3151 35.3553L-164 257.67L-93.2893 328.381L129.026 106.066C148.552 86.5398 148.552 54.8816 129.026 35.3553Z" fill="#00AB44" fill-opacity="0.33"/>
+          <path d="M129.182 212.19C109.655 192.664 77.997 192.664 58.4708 212.19L-68.4336 339.095L2.27708 409.805L129.181 282.901C148.708 263.375 148.708 231.717 129.182 212.19Z" fill="#00AB44" fill-opacity="0.33"/>
+        </svg>
+        <svg width="164" height="325" viewBox="0 0 164 325" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute right-0 top-0 -z-10">
+          <path d="M35.5109 289.45C55.0371 308.977 86.6953 308.977 106.222 289.45L328.537 67.1352L257.826 -3.57543L35.5108 218.74C15.9846 238.266 15.9847 269.924 35.5109 289.45Z" fill="white" fill-opacity="0.5"/>
+          <path d="M35.3551 112.615C54.8813 132.141 86.5396 132.141 106.066 112.615L232.97 -14.2896L162.26 -85.0002L35.3551 41.9042C15.8289 61.4304 15.8289 93.0887 35.3551 112.615Z" fill="white" fill-opacity="0.5"/>
+        </svg>
       </section>
 
-      <section className="py-10 md:px-28">
-        <div className="md:mx-auto space-y-5 mx-3">
-          <div className="flex justify-between items-center space-x-14">
-            <Link href="/" className="cursor-pointer">
-              <Image
-                src="/larven-logo.png"
-                className=""
-                alt="Larven logo"
-                width={1531 / 15}
-                height={1678 / 15}
-                sizes="100vw"
-              />
-            </Link>
-            <ul className="flex space-x-7">
-              <li className="p-2 bg-green-haze-200 rounded-full cursor-pointer hover:bg-green-haze-300">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  fill="currentColor"
-                  className="text-green-haze-700"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z" />
-                </svg>
-              </li>
-              <li className="p-2 bg-green-haze-200 rounded-full cursor-pointer hover:bg-green-haze-300">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  fill="currentColor"
-                  className="text-green-haze-700"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
-                </svg>
-              </li>
-              <li className="p-2 bg-green-haze-200 rounded-full cursor-pointer hover:bg-green-haze-300">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  fill="currentColor"
-                  className="text-green-haze-700"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
-                </svg>
-              </li>
-              <li className="p-2 bg-green-haze-200 rounded-full cursor-pointer hover:bg-green-haze-300">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  fill="currentColor"
-                  className="text-green-haze-700"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
-                </svg>
-              </li>
-            </ul>
-          </div>
-          <hr />
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-y-5 justify-between">
-            <ul className="space-y-3">
-              <li className="text-lg font-bold">Larven</li>
-              <li>Services</li>
-              <li>About</li>
-              <li>Hackathons</li>
-              <li>Careers</li>
-              <li>Core values</li>
-              <li>Ecosystem Partners</li>
-            </ul>
-            <ul className="space-y-3">
-              <li className="text-lg font-bold">Technologies</li>
-              <li>Artificial Intelligence</li>
-              <li>Cloud Computing</li>
-              <li>Computer Experience</li>
-              <li>Aplicaion Services</li>
-              <li>Automations</li>
-              <li>Blockchain Technology</li>
-            </ul>
-            <ul className="space-y-3">
-              <li className="text-lg font-bold">Servises</li>
-              <li>Cyber Security</li>
-              <li>Data & analytics</li>
-              <li>Infrastructure</li>
-              <li>Consulting</li>
-              <li>Training</li>
-              <li>System Auditing</li>
-            </ul>
-          </div>
-        </div>
-      </section>
+      <Footer />
     </>
   );
 };
